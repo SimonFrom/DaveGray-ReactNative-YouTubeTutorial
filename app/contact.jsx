1e603c7b7db427a4ddf970223ac4c034b63a93e5
SimonFrom <simon.bulder@gmail.com>
2026-02-12 21:52:49 +0100

    updates

--- app/contact.jsx
+++ app/contact.jsx
@@ -1,5 +1,6 @@
-import {View, Text, StyleSheet,ImageBackground} from 'react-native'
+import {View, Text, StyleSheet} from 'react-native'
 import React from 'react'
+import { BlurView } from 'expo-blur'
 
 
 
@@ -7,12 +8,23 @@ const contact = () => {
     return (
         <View style={styles.container}>
             <Text style={styles.header}>Welcome to our site!</Text>
+
             <Text style={styles.footer}>
                 Coffee Masters{"\n"}
                 Coffee Lane 12{"\n"}
                 The brown state
             </Text>
+            {/*<View style={styles.wrapper}>*/}
+            {/*    <BlurView style={styles.card} intensity={10} tint="light">*/}
+            {/*        <Text>*/}
+            {/*            Hello there*/}
+            {/*        </Text>*/}
+            {/*    </BlurView>*/}
+            {/*</View>*/}
         </View>
+
+
+
     )
 }
 export default contact
@@ -22,6 +34,29 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         flexDirection: 'column',
+    },
+    wrapper: {
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+        borderRadius: 20,
+        overflow: 'hidden',
+    },
+
+    card: {
+        padding: 16,
+        backgroundColor: 'rgba(255,255,255,0.04)',
+        borderRadius: 20,
+        borderWidth: 1,
+        borderColor: 'rgba(255,255,255,0.25)',
+
+        shadowColor: '#000',
+        shadowOffset: {width: 0, height: 8},
+        shadowOpacity: 0.15,
+        shadowRadius: 18,
+        width: 150,
+
+        elevation: 6, // Android
     },
     header: {
         fontSize: 46,
